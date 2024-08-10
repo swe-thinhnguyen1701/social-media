ad84fb6b85240b73936442d927fa16689fa317de
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-08-10 14:11:19 -0500

    build thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,106 @@
+const Thought = require("../models/Thought");
+const User = require("../models/User")
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find().select("-__v");
+            res.status(200).json(thoughts);
+        } catch (error) {
+            console.error("ERROR occurs while fetching thoughts\n", error);
+            res.status(500).send("Internal error");
+        }
+    },
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId }).select("-__v");
+            if (!thought) return res.status(404).send("Cannot find a thought with given id");
+
+            res.status(200).json(thought);
+        } catch (error) {
+            console.error("ERROR occurs while fetching thought with given id\n", error);
+            res.status(500).send("Internal error");
+        }
+    },
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            const user = await User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $push: { thoughts: thought._id } },
+                { new: true }
+            );
+
+            if (!user) return res.status(404).send("Cannot find user with given id");
+
+            res.status(200).json({ thought, message: "Add new thought success" });
+        } catch (error) {
+            console.error("ERROR occurs while creating new thought\n", error);
+            res.status(500).send("Internal error");
+        }
+    },
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { new: true, runValidators: true }
+            ).select("-__v");
+
+            if (!thought) return res.status(404).send("Cannot find a thought with given id");
+
+            res.status(200).json({ thought, message: "Update thought successfully" });
+        } catch (error) {
+            console.error("ERROR occurs while updating thought\n", error);
+            res.status(500).send("Internal error");
+        }
+    },
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+            if (!thought) return res.status(404).send("Cannot find a thought with given id");
+
+            await User.findOneAndUpdate(
+                { username: thought.username },
+                { $pull: { thoughts: thought._id } });
+
+            res.status(200).json({ thought, message: "Remove a thought success" });
+        } catch (error) {
+            console.error("ERROR occurs while deleting thought\n", error);
+            res.status(500).send("Internal error");
+        }
+    },
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $addToSet: { reactions: req.body } },
+                { new: true, runValidators: true }
+            ).select("-__v");
+
+            if (!thought) return res.status(404).send("Cannot find a thought with given id");
+
+            res.status(200).json({ thought, message: "Add new reaction success" });
+        } catch (error) {
+            console.error("ERROR occurs while adding new reaction\n", error);
+            res.status(500).send("Internal error");
+        }
+    },
+    async deleteReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: { reactions: req.params.reactionId } },
+                { new: true }
+            );
+
+            if (!thought) return res.status(404).send("Cannot find a thought with given id");
+
+            res.status(200).json({ thought, message: "Remove reaction success" });
+        }catch(error) {
+            console.error("ERROR occurs while removing reaction\n", error);
+            res.status(500).send("Internal error");
+        }
+    }
+}
