edf3e8e4db942139dc3e8e76a734e6f471e3141c
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-08-11 11:10:37 -0500

    fix minor error in Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,9 +1,9 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 
 const reactionSchema = new Schema({
     reactionId: {
         type: Schema.Types.ObjectId,
-        default: () => new Schema.Types.ObjectId()
+        default: () => new Types.ObjectId()
     },
     reactionBody: {
         type: String,
