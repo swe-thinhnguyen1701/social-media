8bc972803a33b255a01f4fef21a890ab86857341
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-08-12 14:29:35 -0500

    update user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -69,9 +69,9 @@ module.exports = {
         try {
             const user = await User.findOne({ _id: req.params.userId });
             if (!user) return res.status(404).send("Cannot find user with given id");
-            
+
             const friend = await User.findById(req.params.friendId);
-            if(!friend) return res.status(404).send("Cannot find a friend with given id");
+            if (!friend) return res.status(404).send("Cannot find a friend with given id");
 
             const isFriendExist = user.friends.some(friend => friend._id.toString() === req.params.friendId);
             if (isFriendExist) return res.status(409).send("You already made friend with this user");
@@ -83,8 +83,8 @@ module.exports = {
             ).select("-__v").populate("friends");
 
             await User.updateOne(
-                {_id: friend._id},
-                {$addToSet: {friends: req.params.userId}},
+                { _id: friend._id },
+                { $addToSet: { friends: req.params.userId } },
             )
 
             res.status(200).json({ updateUser, message: "Add new friend success" });
@@ -99,7 +99,7 @@ module.exports = {
             if (!user) return res.status(404).send("Cannot find user with given id");
 
             const friend = await User.findById(req.params.friendId);
-            if(!friend) return res.status(404).send("Cannot find a friend with given id");
+            if (!friend) return res.status(404).send("Cannot find a friend with given id");
 
             const isFriendExist = user.friends.some(friend => friend._id.toString() === req.params.friendId);
             if (!isFriendExist) return res.status(404).send("This user is not your friend yet");
@@ -111,8 +111,8 @@ module.exports = {
             ).select("-__v").populate("friends");
 
             await User.updateOne(
-                {_id: friend._id},
-                {$pull: {friends: req.params.userId}}
+                { _id: friend._id },
+                { $pull: { friends: req.params.userId } }
             )
 
             res.status(200).json({ updateUser, message: "Remove friend success" });
